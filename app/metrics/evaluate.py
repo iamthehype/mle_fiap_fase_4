23610398eeac3eb44c56ec347f2c0c2079357ae8
iamthehype <lucasec911@gmail.com>
2025-06-05 23:15:05 -0300

    Reorganizando o projeto

---
+++ app/metrics/evaluate.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
+def evaluate(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_true, y_pred)
+    return {'MAE': mae, 'MSE': mse, 'RMSE': rmse, 'R2': r2}
